10d48b418ad571df96f658ab21377ff06e2d719e
Whatever Green <43578596+Whatever-Green@users.noreply.github.com>
2021-07-26 16:50:05 +0100

    Update install_script_ubuntu.sh

--- install_script_ubuntu.sh
+++ install_script_ubuntu.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 sudo su 
-apt update -y 
-apt upgrade -y
-apt 
-install snapd
-snap install blender --classic
-snap install krita
-apt install flatpak
-apt install gnome-software-plugin-flatpak
+sudo apt update -y 
+sudo apt upgrade -y 
+sudo apt install snapd
+sudo snap install blender --classic
+sudo snap install krita
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.kde.kdenlive
 
-apt install apt-transport-https curl
+sudo apt install apt-transport-https curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-apt update
-apt install brave-browser
+sudo apt update
+sudo apt install brave-browser
