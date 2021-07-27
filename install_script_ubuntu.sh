bb842fb325596ef62efcfa3a83ecffeeb0ece407
Whatever Green <43578596+Whatever-Green@users.noreply.github.com>
2021-07-27 07:12:13 +0100

    Update install_script_ubuntu.sh

--- install_script_ubuntu.sh
+++ install_script_ubuntu.sh
@@ -1,23 +1,26 @@
-echo Hello welcome, this script will install:
+echo Hello This script was created by,
+echo Whatever Green 
+echo httpswhatevergreen.ml
 echo update the system
 echo install snapd
 echo blender
 echo krita
 echo flatpack
 echo kdenlive
-echo brave
+echo brave 
 sudo apt update -y 
 sudo apt upgrade -y 
 sudo apt install snapd -y 
-sudo snap install blender --classic
-sudo snap install krita
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.kde.kdenlive
+sudo snap install blender --classic -y
+sudo snap install krita -y
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
+flatpak install flathub org.kde.kdenlive -y
 
 sudo apt install apt-transport-https curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+sudo apt update -y
+sudo apt install brave-browser -y
+sudo reboot -y
